3fb96d3b595edb6d7516f43c06ba1686e39998d8
Rohith16-45 <rohithyuvasoft469@gmail.com>
2025-10-14 11:40:55 +0530

    URL changed

--- src/helper/axiosInterceptors.js
+++ src/helper/axiosInterceptors.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://3771290b423e.ngrok-free.app/api/v1/",
+  baseURL: "https://35e11c8600af.ngrok-free.app/api/v1/",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
